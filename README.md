1ef256fecb2736b6cda9a87680481f8974d168d0
jason <jason@glottologist.co.uk>
2020-09-24 19:31:37 +0100

    feat: Added README shields

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
+<!-- omit in TOC -->
 # axon
+
+[![Crates.io](https://img.shields.io/crates/v/axon?style=flat-square)](https://crates.io/crates/axon)
+[![Crates.io](https://img.shields.io/crates/d/axon?style=flat-square)](https://crates.io/crates/axon)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue?style=flat-square)](https://github.com/glottologist/axon/blob/master/LICENSE)
+[![License](https://img.shields.io/badge/license-MIT-blue?style=flat-square)](https://github.com/clap-rs/clap/blob/master/LICENSE-MIT)
+
+
+
 Port of Neuron from Haskell to Rust with additional features
+
+
+[docs]: https://docs.rs/clap
+[examples]: examples
--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,34 @@
 
-use clap::Clap;
+//use clap::Clap;
 
-#[derive(Clap)]
-#[clap(version = "1.0", author = "Glottologist")]
-struct Opts {
+//#[derive(Clap)]
+//#[clap(version = "1.0", author = "Glottologist")]
+//struct Opts {
     
-     #[clap(short,long)]
-     create:bool,
+  //   #[clap(short,long)]
+   //  create:bool,
    
-     #[clap(short,long)]
-     filename: String,
+  //   #[clap(short,long)]
+  //   filename: String,
 
-    #[clap(short,long)]
-     query:String,
+  //  #[clap(short,long)]
+  //   query:String,
 
   
-}
+//}
 
 
 
 fn main() {
 
-    let opts: Opts = Opts::parse();
+  //  let opts: Opts = Opts::parse();
 
-    println!("Value for create: {}", opts.create);
-    println!("Value for ")
+//    println!("Value for create: {}", opts.create);
+    //println!("Value for ")
     
 
 
-    println!("Hello, axon! {}",args.create);
+  //  println!("Hello, axon! {}",args.create);
 }
 
 //fn createZettel() -> String
