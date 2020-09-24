2ad053e3c0bf0b184da22a14291174a169a8cdd2
jason <jason@glottologist.co.uk>
2020-09-24 19:04:51 +0100

    feat: Added skeleton travis file

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+
+use clap::Clap;
+
+#[derive(Clap)]
+#[clap(version = "1.0", author = "Glottologist")]
+struct Opts {
+    
+     #[clap(short,long)]
+     create:bool,
+   
+     #[clap(short,long)]
+     filename: String,
+
+    #[clap(short,long)]
+     query:String,
+
+  
+}
+
+
+
+fn main() {
+
+    let opts: Opts = Opts::parse();
+
+    println!("Value for create: {}", opts.create);
+    println!("Value for ")
+    
+
+
+    println!("Hello, axon! {}",args.create);
+}
+
+//fn createZettel() -> String
